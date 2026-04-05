ce3f38dbcef019751f354d16c9b99ae0e5efebd3
arryllopez <arryllopez7@gmail.com>
2026-04-04 22:32:18 -0400

    implementing gu2gl wrapper header

--- src/main.c
+++ src/main.c
@@ -10,14 +10,18 @@
 #include <stdlib.h> 
 #include <stdio.h>
 
+// importing the gu2gl header 
+#define GU2GL_IMPLEMENTATION
+#include "gu2gl.h"
+
 // PSP_MODULE_INFO is required
 PSP_MODULE_INFO("Hello World", 0, 1, 0);
 PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_USER);
 
-// Definition of PSP Width and Height
-#define PSP_BUF_WIDTH (512) 
-#define PSP_SCR_WIDTH (480)
-#define PSP_SCR_HEIGHT (272)
+// // Definition of PSP Width and Height
+// #define PSP_BUF_WIDTH (512) 
+// #define PSP_SCR_WIDTH (480)
+// #define PSP_SCR_HEIGHT (272)
 
 // Global Variable that starts the game loop
 int running = 1;
